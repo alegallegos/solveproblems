f9d082fb23c5c4327f7e7f9f03fc7120ca53f640
Alejandro Gallegos <alejandro.gallegos@otcxn.com>
2019-07-25 13:05:15 -0300

    Contains duplicate: more complex example

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,18 +8,10 @@ public class App {
 //    int[] array = {1,2,2,3};
 //    System.out.println(majorityElement.process(array));
 
-//    AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
-//    int[] nums1 = {2 ,4, 3};
-//    AddTwoNumbers.ListNode list1 = addTwoNumbers.createList(nums1);
-//    int[] nums2 = {5, 6, 4};
-//    AddTwoNumbers.ListNode list2 = addTwoNumbers.createList(nums2);
-//
-//    System.out.println(addTwoNumbers.process(list1, list2));
+    int[] array = {1,2,2,1};
 
-    ContainsDuplicate containsDuplicate = new ContainsDuplicate();
-    int[] array = {0,2,6,4,2,8,9,1};
-    System.out.println(containsDuplicate.containsDuplicate(array));
-    System.out.println(containsDuplicate.containsDuplicate2(array));
+    System.out.println(ContainsDuplicateII.process(array, 2));
+
   }
 
 }
--- src/main/java/ContainsDuplicateII.java
+++ src/main/java/ContainsDuplicateII.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.io.IOException;
 
+//Given an array of integers and an integer k, find out whether there are two distinct indices i and
+//j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
 public class ContainsDuplicateII {
+
+  public static boolean process(int[] nums, int k) {
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = 0; j < nums.length; j++) {
+        if(nums[i] == nums[j] && Math.abs(i - j) > k) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
 }
